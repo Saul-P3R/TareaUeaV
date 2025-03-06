af5d888a95f6aaf91c14465eaefc9cfc13e0421c
Saul-P3R <straydogs567@gmail.com>
2025-03-06 17:35:53 -0600

    Add files via upload

---
+++ Actividadesclaseuea/Critica a tecnologias web/script.js
@@ -0,0 +1,7 @@
+let p1 ="La tecnología web (HTML, CSS, JS) es importante para mi formación como artista digital. Me permite crear experiencias visuales y interactivas en línea que son increíbles y dinamicas. Con HTML, puedo estructurar y organizar el contenido de mis proyectos, mientras que con CSS, puedo darles un toque personalizado y hacer que se vean geniales. Y con JavaScript, puedo agregar interactividad y hacer que mis proyectos sean aún más divertidos.";
+let p2 ="Además, la tecnología web me permite desarrollar habilidades en diseño y desarrollo de interfaces de usuario (UI) y experiencias de usuario (UX). Esto es fundamental para crear proyectos digitales que sean fáciles de usar y que la gente disfrute. La tecnología web también me permite colaborar con otros artistas y desarrolladores, compartir ideas y aprender de sus experiencias. En resumen, la tecnología web es una herramienta clave para mi formación como artista digital, y me permite crear proyectos innovadores y divertidos.";
+
+
+document.getElementById('parr1').innerHTML="parrafo 1".innerHTML=p1;
+
+document.getElementById('parr2').innerHTML="parrafo 2".innerHTML=p2;
---
+++ Actividadesclaseuea/tareas 2/script.js
@@ -0,0 +1,7 @@
+let p1 ="parrafo 1 escribir aqui";
+let p2 ="parrafo 2 escribir aca asdhkashdkjashdas";
+
+
+document.getElementById('parr1').innerHTML="parrafo 1".innerHTML=p1;
+
+document.getElementById('parr2').innerHTML="parrafo 2".innerHTML=p2;

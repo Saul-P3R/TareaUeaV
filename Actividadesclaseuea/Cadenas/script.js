ff86302d44b224bae7fa4ab6b45187bca31eeadf
Saul-P3R <straydogs567@gmail.com>
2025-04-03 12:12:18 -0600

    Add files via upload

---
+++ Actividadesclaseuea/Cadenas/script.js
@@ -0,0 +1,11 @@
+let txt = " Por la supertierra.";
+let txt2=txt.toUpperCase();
+let txt3=txt2.replace("SUPERTIERRA.","DEMOCRACIA");
+
+document.getElementById('cadena').innerHTML=txt;
+document.getElementById('cadena2').innerHTML=txt2;
+document.getElementById('cadena3').innerHTML=txt3;
+
+//console.log(txt.length)
+//console.log(txt. at(18))
+console.log(txt. toLocaleUpperCase())
